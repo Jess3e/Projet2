c5ef8640d7797f200bc7f4bdf375ca7f0a883294
Jessee Lefebvre <jessee.lefebvre@gmail.com>
2016-10-17 15:47:03 -0400

    Allo

--- app/src/main/java/com/example/jess3e/projet2/MainActivity.java
+++ app/src/main/java/com/example/jess3e/projet2/MainActivity.java
@@ -1,20 +1,9 @@
 package com.example.jess3e.projet2;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.FaceDetector;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -26,6 +15,8 @@ public class MainActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.setContentView(R.layout.activity_main);
+        image = (ImageView)findViewById(R.id.imageView2);
+        image.setImageResource(R.drawable.titre);
 
     }
 
