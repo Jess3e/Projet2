7d9c496e20fb242be07944683a1110beeac5d8d2
Jessee Lefebvre <jessee.lefebvre@gmail.com>
2016-10-17 15:38:30 -0400

    Allo

--- app/src/main/java/com/example/jess3e/projet2/MainActivity.java
+++ app/src/main/java/com/example/jess3e/projet2/MainActivity.java
@@ -25,23 +25,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.setContentView(R.layout.your_layout_name_here);
+        this.setContentView(R.layout.activity_main);
+
     }
 
-    public void onDraw(Canvas canvas) {
-        canvas.drawBitmap(background_image, 0, 0, null);
-        for (int i = 0; i < face_count; i++) {
-            FaceDetector.Face face = faces[i];
-            tmp_paint.setColor(Color.GREEN);
-            tmp_paint.setAlpha(100);
-            face.getMidPoint(tmp_point);
-            canvas.drawCircle(tmp_point.x, tmp_point.y, face.eyesDistance(),
-                    tmp_paint);
-        }
-    }
-
-    public void closeActivity(View view) {
-        finish();
-    }
 }
 
